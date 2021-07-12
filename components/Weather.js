c2cfa8e4d0c2b9cb9b2fedc9c71b6a7ed341ac61
6210110040 <6210110040@psu.ac.th>
2021-07-12 12:21:50 +0700

    Part05 v0.6

--- components/Weather.js
+++ components/Weather.js
@@ -1,6 +1,6 @@
 import React, { useState , useEffect } from "react";
 import Forecast from "./Forecast";
-import { ImageBackground, StyleSheet, Text , View } from "react-native";
+import { ImageBackground, StatusBar, StyleSheet, Text , View } from "react-native";
 
 
 export default function Weather(props) {
